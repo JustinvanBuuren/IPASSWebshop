0c7af0f04ea7af0a2058d5d4770f92f7b1d2222f
Justin <jjpieters1@hotmail.com>
2019-07-09 20:38:23 +0200

    Nieuwe game toevoegen en ophalen game met id en ophalen alle games werkend in backend

---
+++ api/src/main/java/nl/ipass/definition/Game.java
@@ -0,0 +1,94 @@
+package nl.ipass.definition;
+
+public class Game {
+    private String publicId;
+    private String titel;
+    private BestelStatus bestelStatus;
+    private Double prijs;
+    private String genre;
+    private String platform;
+    private Integer voorraad;
+
+    public String getPublicId() {
+        return this.publicId;
+    }
+
+    public void setPublicId(String publicId) throws IllegalArgumentException {
+        if(publicId.length() != 256) {
+            throw new IllegalArgumentException("Ongeldige public id.");
+        }
+        this.publicId = publicId;
+    }
+
+    public String getTitel() {
+        return titel;
+    }
+
+    public void setTitel(String titel) throws IllegalArgumentException {
+        if(titel.length() == 0 || titel.length() > 128) {
+            throw new IllegalArgumentException("De titel mag niet leeg zijn en mag niet langer zijn dan 128 karakters.");
+        }
+        this.titel = titel;
+    }
+
+    public BestelStatus getBestelStatus() {
+        return bestelStatus;
+    }
+
+    public void setBestelStatus(BestelStatus bestelStatus) {
+        this.bestelStatus = bestelStatus;
+    }
+
+    public Double getPrijs() {
+        return prijs;
+    }
+
+    public void setPrijs(Double prijs) throws IllegalArgumentException {
+        if(prijs < 0) {
+            throw new IllegalArgumentException("De prijs mag niet lager zijn dan 0.");
+        }
+        this.prijs = prijs;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) throws IllegalArgumentException {
+        if(genre.length() == 0 || genre.length() > 64) {
+            throw new IllegalArgumentException("De genre mag niet langer zijn en mag niet langer zijn dan 64 karakters.");
+        }
+        this.genre = genre;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) throws IllegalArgumentException {
+        if(platform.length() == 0 || platform.length() > 64) {
+            throw new IllegalArgumentException("Platform mag niet leeg zijn en mag niet langer zijn dan 64 karakters.");
+        }
+        this.platform = platform;
+    }
+
+    public Integer getVoorraad() {
+        return voorraad;
+    }
+
+    public void setVoorraad(Integer voorraad) throws IllegalArgumentException {
+        if(voorraad < 0) {
+            throw new IllegalArgumentException("Voorraad mag niet lager zijn dan 0.");
+        }
+        this.voorraad = voorraad;
+    }
+
+    public String toString() {
+        return "@Game:[titel: " + getTitel()+
+                ", bestelStatus: " + getBestelStatus() +
+                ", prijs: " + getPrijs() +
+                ", genre: " + getGenre() +
+                ", platform: " + getPlatform() +
+                ", voorraad: " + getVoorraad();
+    }
+}
--- api/src/main/java/nl/ipass/endpoint/GamesEndpoint.java
+++ api/src/main/java/nl/ipass/endpoint/GamesEndpoint.java
@@ -1,60 +1,48 @@
 package nl.ipass.endpoint;
 
-import nl.ipass.definition.ipass.Game;
+import nl.ipass.definition.Game;
+import nl.ipass.service.GameService;
 
+import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import java.sql.*;
+import java.util.List;
 
 @Path("games")
 public class GamesEndpoint {
 
-    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost:3306/ipass";
-    static final String USER = "ipass";
-    static final String PASS = "ipass";
+    private GameService gameService;
+
+    public GamesEndpoint() {
+        // Deze constructor is bewust leeg gelaten
+    }
+
+    @Inject
+    public GamesEndpoint(GameService gameService) {
+        this.gameService = gameService;
+    }
+
 
     @GET
-    @Produces("application/json")
-    public Game getAllGames() {
-        Game game = new Game();
-        game.setBestelStatus("1");
-        game.setGenre("2");
-        game.setPlatform("3");
-        game.setPrijs(10.00);
-        game.setTitel("4");
-        game.setVoorraad(15);
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Game> findAllGames() {
+        return gameService.findAll();
+    }
 
-
-        return game;
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Game findGameById(@PathParam("id") String id) {
+        return gameService.findById(id);
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Game nieuweGame(Game game) {
+        gameService.save(game);
 
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-
-            Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-            Statement stmt = conn.createStatement();
-            String query = "INSERT INTO game (titel, bestelStatus, prijs, genre, platform, voorraad) VALUES (?, ?, ?, ?, ?, ?)";
-            PreparedStatement preparedStatement = conn.prepareStatement(query);
-            preparedStatement.setString(1, game.getTitel());
-            preparedStatement.setString(2, game.getBestelStatus());
-            preparedStatement.setDouble(3, game.getPrijs());
-            preparedStatement.setString(4, game.getGenre());
-            preparedStatement.setString(5, game.getPlatform());
-            preparedStatement.setInt(6, 10);
-
-            boolean result = preparedStatement.execute();
-
-            conn.close();
-
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
         return game;
     }
+
 }
